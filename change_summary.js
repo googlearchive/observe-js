298c9aa8260ba5f3dc28da4076ae31f205585447
Rafael Weinstein <rafaelw@chromium.org>
2013-02-21 19:12:54 -0800

    implemented two-way binding. unbind not yet implemented

--- change_summary.js
+++ change_summary.js
@@ -373,12 +373,15 @@
   function ChangeSummary(externalCallback) {
     var observing = true;
     var isDisconnecting = false;
+    var isDelivering = false;
     var changesDelivered = false;
     var summaries;
 
     var internal = {
 
       objectObservers: new Map,
+
+      bindingPriorityCounter: 1,
 
       getObjectObserver: function(obj) {
         return this.objectObservers.get(obj);
@@ -396,6 +399,10 @@
 
       removeObjectObserver: function(obj) {
         this.objectObservers.delete(obj);
+      },
+
+      nextBindingPriority: function() {
+        return this.bindingPriorityCounter++;
       },
 
       addPathTracker: function(obj, pathTracker) {
@@ -426,7 +433,8 @@
         }
 
         try {
-          this.activeObservers = new Set;
+          if (!isDelivering)
+            this.activeObservers = new Set;
 
           records.forEach(function(record) {
             var observer = this.getObjectObserver(record.object);
@@ -434,7 +442,12 @@
             this.activeObservers.add(observer);
           }, this);
 
+          if (isDelivering)
+            return;
+
+          isDelivering = true;
           this.deliverSummaries();
+          isDelivering = false;
 
         } catch (ex) {
           console.error(ex);
@@ -455,11 +468,43 @@
         } while (changesDelivered && cycles < MAX_DIRTY_CHECK_CYCLES)
       },
 
+      resolveBindings: function() {
+        var methods = this.planner.getPlan();
+
+        if (!methods)
+          console.error('Error: bindings are over-constrained');
+
+        var changed = false;
+        for (var i = 0; i < methods.length; i++) {
+          var method = methods[i];
+          var pathTracker = method.to;
+          if (!pathTracker.setValue(method.from.value))
+            break;
+
+          changed = true;
+          var observer = this.getOrCreateObjectObserver(pathTracker.object);
+          observer.addDirtyPath(pathTracker);
+          this.activeObservers.add(observer);
+        }
+
+        return changed;
+      },
+
       deliverSummaries: function() {
         summaries = [];
 
+        var shouldResolveBindings = false;
         this.activeObservers.forEach(function(observer) {
-          observer.process();
+          shouldResolveBindings = observer.checkPathValues() || shouldResolveBindings;
+        });
+
+        if (shouldResolveBindings) {
+          if (this.resolveBindings() && hasObserve)
+            Object.deliverChangeRecords(internal.callback);
+        }
+
+        this.activeObservers.forEach(function(observer) {
+          observer.checkObjectsAndArrays();
         });
 
         this.activeObservers.forEach(function(observer) {
@@ -493,6 +538,9 @@
       Object.observe(register, internal.callback);
       Object.unobserve(register, internal.callback);
     }
+
+    // TODO(rafaelw): Disallow changing observation/bindings while connected.
+    // TODO(rafaelw): Garbage collection???
 
     this.observeObject = function(obj) {
       if (!isObject(obj))
@@ -541,7 +589,8 @@
       if (!isObject(obj))
         return undefined;
 
-      return internal.getOrCreateObjectObserver(obj).observePath(path);
+      var pathTracker = internal.getOrCreateObjectObserver(obj).observePath(path);
+      return pathTracker.value;
     };
 
     this.unobservePath = function(obj, pathString) {
@@ -555,7 +604,82 @@
       if (!isObject(obj))
         return;
 
-      internal.getObjectObserver(obj).unobservePath(path);
+      var observer = internal.getObjectObserver(obj)
+      if (!observer)
+        return;
+
+      observer.unobservePath(path);
+    };
+
+    this.bind = function(obj1, pathString1, obj2, pathString2) {
+      if (typeof global.Planner !== 'function')
+        throw Error('Binding requires Planner library');
+
+      // TODO(rafaelw): Implement all the non-observing and error cases.
+      if (!isPathValid(pathString1) || !isPathValid(pathString2))
+        throw Error('Not implemented');
+
+      var path1 = new Path(pathString1);
+      var path2 = new Path(pathString2);
+      if (!path1.length || !path2.length || !isObject(obj1) || !isObject(obj2))
+        throw Error('Not implemented');
+
+      if (!internal.planner)
+        internal.planner = new Planner();
+
+      var pathTracker1 = internal.getOrCreateObjectObserver(obj1).observePath(path1, true);
+      var pathTracker2 = internal.getOrCreateObjectObserver(obj2).observePath(path2, true);
+
+      if (pathTracker2.bindings && tracker2.bindings.has(pathTracker1))
+        return; // TODO(rafaelw): reverse binding attempted. ok to just return?
+
+      if (pathTracker1.bindings && pathTracker1.bindings.has(pathTracker2))
+        return;
+
+      if (!pathTracker1.bindings)
+        pathTracker1.bindings = new Map;
+
+      // TODO(rafaelw): Having variables hold references to data may make
+      // garbage collection harder
+      if (!pathTracker1.variable) {
+        var priority1 = internal.nextBindingPriority();
+        pathTracker1.variable = internal.planner.addVariable(function() {
+          return pathTracker1.changed ? priority1 : Infinity;
+        });
+      }
+
+      if (!pathTracker2.variable) {
+        var priority2 = internal.nextBindingPriority();
+        pathTracker2.variable = internal.planner.addVariable(function() {
+          return pathTracker2.changed ? priority2 : Infinity;
+        });
+      }
+
+      var constraint = internal.planner.addConstraint();
+      pathTracker1.bindings.set(pathTracker2, constraint);
+
+      var toMethod = constraint.addMethod(pathTracker2.variable);
+      var fromMethod = constraint.addMethod(pathTracker1.variable);
+
+      toMethod.from = pathTracker1;
+      toMethod.to = pathTracker2;
+      toMethod.name = pathTracker2.path.toString() + '->' + pathTracker1.path.toString();
+
+      fromMethod.from = pathTracker2;
+      fromMethod.to = pathTracker1;
+      toMethod.name = pathTracker1.path.toString() + '->' + pathTracker2.path.toString();
+
+      if (pathTracker1.value === pathTracker2.value)
+        return;
+
+      if (pathTracker1.value === undefined)
+        pathTracker1.setValue(pathTracker2.value);
+      else
+        pathTracker2.setValue(pathTracker1.value);
+    };
+
+    this.unbind = function(obj1, pathString1, obj2, pathString2) {
+      throw Error('Not implemented');
     };
 
     this.deliver = function() {
@@ -563,7 +687,7 @@
         Object.deliverChangeRecords(internal.callback);
       else
         internal.dirtyCheck();
-    }
+    };
 
     this.disconnect = function() {
       if (!observing)
@@ -613,6 +737,19 @@
     return retval;
   }
 
+  function internalSetValueAtPath(obj, path, value) {
+    var changed = false;
+
+    path.walkPropertiesFrom(obj, function(prop, m, i) {
+      if (isObject(m) && i == path.length - 1) {
+        changed = true;
+        m[prop] = value;
+      }
+    });
+
+    return changed;
+  }
+
   ChangeSummary.setValueAtPath = function(obj, pathString, value) {
     if (!isPathValid(pathString))
       return;
@@ -624,10 +761,7 @@
     if (!isObject(obj))
       return;
 
-    path.walkPropertiesFrom(obj, function(prop, m, i) {
-      if (isObject(m) && i == path.length - 1)
-        m[prop] = value;
-    });
+    internalSetValueAtPath(obj, path, value);
   };
 
   ChangeSummary.applySplices = function(previous, current, splices) {
@@ -798,7 +932,11 @@
     this.pathString = pathString;
     this.observed = path.length > 1 ? new Array(path.length - 2) : undefined;
     this.changed = false;
-    this.newValue = undefined;
+    this.oldValue = undefined;
+    this.bound = false;
+    this.reporting = false;
+    this.bindings = undefined;
+    this.variable = undefined;
 
     this.internal = internal;
 
@@ -816,9 +954,10 @@
 
   if (hasObserve) {
     pathTrackerCheck = function() {
+      var newValue;
       this.path.walkPropertiesFrom(this.object, function(prop, value, i) {
         if (i === this.path.length) {
-          this.newValue = value;
+          newValue = value;
           return;
         }
 
@@ -850,7 +989,7 @@
         this.internal.addPathTracker(observed, this);
       }, this);
 
-      return this.changed = this.value !== this.newValue;
+      return this.isChanged(newValue);
     };
   } else if (hasEval) {
     pathTrackerCheck = function() {
@@ -873,34 +1012,48 @@
         this.checkFunc = new Function('obj', str);
       }
 
-      this.newValue = this.checkFunc(this.object);
-      return this.changed = this.value !== this.newValue;
+      return this.isChanged(this.checkFunc(this.object));
     };
 
   } else {
     pathTrackerCheck = function() {
+      var newValue;
       this.path.walkPropertiesFrom(this.object, function(prop, value, i) {
         if (i === this.path.length)
-          this.newValue = value;
+          newValue = value;
       }, this);
 
-      return this.changed = this.value !== this.newValue;
+      return this.isChanged(newValue);
     };
   }
 
   PathTracker.prototype = {
     check: pathTrackerCheck,
 
+    isChanged: function(newValue) {
+      if (!this.changed)
+        this.oldValue = this.value;
+      this.value = newValue;
+      return this.changed = this.oldValue !== this.value;
+    },
+
+    setValue: function(newValue) {
+      var changed = internalSetValueAtPath(this.object, this.path, newValue);
+      if (!changed)
+        return false;
+      this.isChanged(newValue);
+      return changed;
+    },
+
     summarize: function(summary, oldValues) {
-      summary.pathChanged[this.pathString] = this.newValue;
-      oldValues[this.pathString] = this.value;
+      summary.pathChanged[this.pathString] = this.value;
+      oldValues[this.pathString] = this.oldValue;
     },
 
     reset: function(force) {
       if (force)
         this.check();
-      this.value = this.newValue;
-      this.newValue = undefined;
+      this.oldValue = undefined;
       this.changed = false;
     },
 
@@ -924,6 +1077,24 @@
   }
 
   ObjectObserver.prototype = {
+    observeObject: function() {
+      this.objectTracker = new ObjectTracker(this.object);
+    },
+
+    unobserveObject: function() {
+      this.objectTracker = undefined;
+      this.destroyIfEmpty();
+    },
+
+    observeArray: function() {
+      this.arrayTracker = new ArrayTracker(this.object);
+    },
+
+    unobserveArray: function() {
+      this.arrayTracker = undefined;
+      this.destroyIfEmpty();
+    },
+
     addPathTracker: function(pathTracker, opt_pathString) {
       if (!this.pathTrackers) {
         this.pathTrackers = [];
@@ -947,29 +1118,42 @@
       this.destroyIfEmpty();
     },
 
-    observePath: function(path) {
+    observePath: function(path, forBinding) {
       var pathTracker;
+      var pathString = path.toString();
       if (this.pathTrackers && this.pathTrackerMap)
         pathTracker = this.pathTrackerMap[pathString];
 
       if (pathTracker) {
-        pathTracker.reset();
+        pathTracker.check();
       } else {
-        var pathString = path.toString();
         pathTracker = new PathTracker(this.object, path, pathString, this.internal);
         this.addPathTracker(pathTracker, pathString);
       }
 
-      return pathTracker.value;
+      if (forBinding)
+        pathTracker.bound = true;
+      else
+        pathTracker.reporting = true;
+
+      return pathTracker;
     },
 
-    unobservePath: function(path) {
+    unobservePath: function(path, forBinding) {
       if (!this.pathTrackerMap)
         return;
 
       var pathString = path.toString();
       var pathTracker = this.pathTrackerMap[pathString];
       if (!pathTracker)
+        return;
+
+      if (forBinding)
+        pathTracker.bound = forBinding;
+      else
+        pathTracker.reporting = false
+
+      if (pathTracker.bound || pathTracker.reporting)
         return;
 
       pathTracker.destroy();
@@ -982,24 +1166,6 @@
       if (!this.pathTrackers.length)
         this.pathTrackers = undefined;
 
-      this.destroyIfEmpty();
-    },
-
-    observeObject: function() {
-      this.objectTracker = new ObjectTracker(this.object);
-    },
-
-    unobserveObject: function() {
-      this.objectTracker = undefined;
-      this.destroyIfEmpty();
-    },
-
-    observeArray: function() {
-      this.arrayTracker = new ArrayTracker(this.object);
-    },
-
-    unobserveArray: function() {
-      this.arrayTracker = undefined;
       this.destroyIfEmpty();
     },
 
@@ -1036,21 +1202,14 @@
       this.changeRecords.push(changeRecord);
     },
 
-    process: function() {
+    checkPathValues: function(changeRecords) {
       if (!this.internal)  // observation stopped mid-process. TODO(rafaelw): Is this really possible?
         return;
 
-      if (this.objectTracker)
-        this.objectTracker.check(this.changeRecords);
-      if (this.arrayTracker)
-        this.arrayTracker.check(this.changeRecords);
-
-      this.checkPathTrackers(this.changeRecords);
-    },
-
-    checkPathTrackers: function() {
       if (!this.pathTrackers)
         return;
+
+      var bindingsAreDirty = false;
 
       for (var i = 0; i < this.pathTrackers.length; i++) {
         var pathTracker = this.pathTrackers[i];
@@ -1060,8 +1219,23 @@
           observer.addDirtyPath(pathTracker);
           if (!isThis)
             this.internal.activeObservers.add(observer);
+
+          if (pathTracker.variable)
+            bindingsAreDirty = true;
         }
       }
+
+      return bindingsAreDirty;
+    },
+
+    checkObjectsAndArrays: function() {
+      if (!this.internal)  // observation stopped mid-process. TODO(rafaelw): Is this really possible?
+        return;
+
+      if (this.objectTracker)
+        this.objectTracker.check(this.changeRecords);
+      if (this.arrayTracker)
+        this.arrayTracker.check(this.changeRecords);
     },
 
     addDirtyPath: function(pathTracker) {
@@ -1091,16 +1265,28 @@
       if (this.pathTrackerMap)
         summary.pathChanged = {};
 
-      if (this.objectTracker)
+      var changed = false;
+      if (this.objectTracker) {
         this.objectTracker.summarize(summary, oldValues);
-      if (this.arrayTracker)
+        changed = changed || this.objectTracker.changed;
+      }
+
+      if (this.arrayTracker) {
         this.arrayTracker.summarize(summary);
+        changed = changed || this.arrayTracker.changed;
+      }
+
       if (this.dirtyPathTrackers) {
         this.dirtyPathTrackers.forEach(function(pathTracker) {
+          if (!pathTracker.changed || !pathTracker.reporting)
+            return;
+
           pathTracker.summarize(summary, oldValues);
+          changed = true;
         });
       }
 
+      if (changed)
       return summary;
     },
 
